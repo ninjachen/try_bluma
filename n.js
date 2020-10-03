68508c1135ea8d327e55e663c0c93cd7e2629897
Ninja <jiachenning@gmail.com>
2020-10-03 16:19:30 +0800

    Archive after chart finished

--- n.js
+++ n.js
@@ -1,4 +1,5 @@
-var end_point_url = "http://localhost:8000";
+var end_point_url = "http://35.194.149.184/api";
+var token = window.location.hash ? null : window.location.hash.substr(1);
 
 let common_request = obj => {
 	return new Promise((resolve, reject) => {
@@ -45,6 +46,9 @@ function renderLisit(resp, template, list_continer) {
 		title_dom.setAttribute("href", url);
 		c.querySelector("#q_view_cnt_temp").innerHTML = viewCnt;
 		c.querySelector("#q_cre_date_temp").innerHTML = credate;
+		c.querySelector("#rm_question_btn").onclick = () => rm_question(url);
+		c.querySelector("#chart_btn").onclick = () => fetch_chart_api(url);
+		
 		list_continer.appendChild(c);
 	});
 }
@@ -60,7 +64,7 @@ function init_listener () {
 			method: "post", 
 			url: add_url, 
 			body: {
-				token: "674b514b0fe47c51ce416044bad2423a",
+				token: token,
 				url:question_url,
 				ninja:"chen"}})
 		.then(resp => {
@@ -79,8 +83,11 @@ function init_listener () {
 }
 
 function refresh_list_api() {
+	if(!token) {
+		alert("没有登陆");
+	}
 	// list api
-	var list_url = end_point_url + "/q/list?token=674b514b0fe47c51ce416044bad2423a";
+	var list_url = end_point_url + "/q/list?token=" + token;
 	common_request({url: list_url})
 	.then(resp => {
 		console.log("promise finished, resp is ", resp)
@@ -96,7 +103,103 @@ function refresh_list_api() {
 	});
 }
 
+/**
+ * 删除问题
+ * @param {a} url 
+ */
+function rm_question(url) {
+	console.log("rm_question..");
+	if(!token) {
+		alert("没有登陆");
+	}
+	// list api
+	var url = end_point_url + "/q/rm?token=" + token + "&url=" + url;
+	common_request({url: url})
+	.then(resp => {
+		console.log("promise finished, resp is ", resp)
+		if(resp.code !== null && resp.code === 0) {
+			alert("删除问题成功");
+			renderLisit(resp, document.querySelector("#table_row_temp"), document.querySelector("#table_body"))
+		} else {
+			if(resp && resp.msg) {
+				alert(resp.msg);
+			}else {
+				alert("删除问题失败");
+			}
+		}
+	});
+}
+
+function fetch_chart_api(question_url) {
+	console.log("fetch_chart_api..");
+	if(!token) {
+		alert("没有登陆");
+	}
+	// list api
+	question_url = encodeURIComponent(question_url);
+	var url = end_point_url + "/q/question_trend?token=" + token + "&url=" + question_url;
+	common_request({url: url})
+	.then(resp => {
+		console.log("promise finished, resp is ", resp)
+		if(resp.code !== null && resp.code === 0) {
+			alert("获取图标接口成功");
+			// renderChart(resp.data, document.querySelector("#chart_container"));
+		} else {
+			if(resp && resp.msg) {
+				alert(resp.msg);
+			}else {
+				alert("请求列表接口失败");
+			}
+		}
+	});
+}
+
+function renderChart(chartData, dom) {
+	// var xList = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+	var xList = chartData.x_list;
+	// var dataList = [820, 932, 901, 934, 1290, 1330, 1320];
+	var dataList = chartData.data_list;
+
+	var mychart  = echarts.init(dom);
+	var option = {
+		tooltip: {
+			triger: "axis",
+			axisPointer: {
+				type: "cross",
+				crossStyle: {
+					color: "#999"
+				}
+			}
+		},
+		xAxis: {
+			type: 'category',
+			boundaryGap: false,
+			data: xList
+		},
+		yAxis: {
+			type: 'value',
+			name: "浏览量"
+		},
+		series: [{
+			data: dataList,
+			type: 'line',
+			areaStyle: {}
+		}]
+	};
+	mychart.setOption(option, true);
+}
+
+
 function main() {
+	token = (!window.location.hash ? null : window.location.hash.substr(1));
+	console.log(token);
+	console.log(window.location.hash);
+	if(window.location.hash) {
+		console.log("has hash");
+	}
+	if(!token) {
+		console.log("ninjas没有登陆");
+	}
 	// init listener
 	init_listener();
 	refresh_list_api();
